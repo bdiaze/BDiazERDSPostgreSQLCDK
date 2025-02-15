2f60248818711d1219e1381fc15107d7e167524b
Benjamín <benjamin.diaze@outlook.com>
2025-02-15 16:37:52 -0300

    Initial commit

---
+++ src/BDiazErdsPostgreSql/BDiazErdsPostgreSqlStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace BDiazErdsPostgreSql
+{
+    public class BDiazErdsPostgreSqlStack : Stack
+    {
+        internal BDiazErdsPostgreSqlStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
