93efd18bfcd4e0b09f6847b7d00fd307d06dba6f
Benjamín <benjamin.diaze@outlook.com>
2025-02-22 17:03:08 -0300

    Se añade Secret con información de la instancia, y con información adicional a subaplicaciones

--- src/BDiazErdsPostgreSql/BDiazErdsPostgreSqlStack.cs
+++ src/BDiazErdsPostgreSql/BDiazErdsPostgreSqlStack.cs
@@ -3,15 +3,17 @@ using Amazon.CDK.AWS.EC2;
 using Amazon.CDK.AWS.IAM;
 using Amazon.CDK.AWS.Logs;
 using Amazon.CDK.AWS.RDS;
+using Amazon.CDK.AWS.SecretsManager;
+using Amazon.CDK.AWS.SSM;
 using Constructs;
+using System.Collections.Generic;
 using InstanceType = Amazon.CDK.AWS.EC2.InstanceType;
 
 namespace BDiazErdsPostgreSql
 {
     public class BDiazERDSPostgreSqlStack : Stack
     {
-        internal BDiazERDSPostgreSqlStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
-        {
+        internal BDiazERDSPostgreSqlStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props) {
             string appName = System.Environment.GetEnvironmentVariable("APP_NAME")!;
             string vpcId = System.Environment.GetEnvironmentVariable("VPC_ID")!;
             string subnetId1 = System.Environment.GetEnvironmentVariable("SUBNET_ID_1")!;
@@ -20,6 +22,17 @@ namespace BDiazErdsPostgreSql
             string masterPassword = System.Environment.GetEnvironmentVariable("MASTER_PASSWORD")!;
             string lambdaSecurityGroupId = System.Environment.GetEnvironmentVariable("LAMBDA_SECURITY_GROUP_ID")!;
             string ec2SecurityGroupId = System.Environment.GetEnvironmentVariable("EC2_SECURITY_GROUP_ID")!;
+
+            // Se obtienen passwords de las otras aplicaciones para almacenar en el mismo secret...
+            string subapp01Name = System.Environment.GetEnvironmentVariable("SUBAPP_01_NAME")!;
+            string subapp01Database = System.Environment.GetEnvironmentVariable("SUBAPP_01_DATABASE")!;
+            string subapp01Username = System.Environment.GetEnvironmentVariable("SUBAPP_01_USERNAME")!;
+            string subapp01Password = System.Environment.GetEnvironmentVariable("SUBAPP_01_PASSWORD")!;
+
+            string subapp02Name = System.Environment.GetEnvironmentVariable("SUBAPP_02_NAME")!;
+            string subapp02Database = System.Environment.GetEnvironmentVariable("SUBAPP_02_DATABASE")!;
+            string subapp02Username = System.Environment.GetEnvironmentVariable("SUBAPP_02_USERNAME")!;
+            string subapp02Password = System.Environment.GetEnvironmentVariable("SUBAPP_02_PASSWORD")!;
 
             IVpc vpc = Vpc.FromLookup(this, $"{appName}Vpc", new VpcLookupOptions {
                 VpcId = vpcId
@@ -48,7 +61,7 @@ namespace BDiazErdsPostgreSql
             securityGroup.AddIngressRule(Peer.SecurityGroupId(ec2SecurityGroupId), Port.POSTGRES, "Ingress para instancias EC2");
 
 
-            ParameterGroup parameterGroup = new ParameterGroup(this, $"{appName}ParameterGroup", new ParameterGroupProps { 
+            ParameterGroup parameterGroup = new(this, $"{appName}ParameterGroup", new ParameterGroupProps {
                 Engine = DatabaseInstanceEngine.Postgres(new PostgresInstanceEngineProps {
                     Version = PostgresEngineVersion.VER_17_2
                 }),
@@ -58,12 +71,12 @@ namespace BDiazErdsPostgreSql
             IRole role = new Role(this, $"{appName}RDSMonitoringRole", new RoleProps {
                 AssumedBy = new ServicePrincipal("monitoring.rds.amazonaws.com"),
                 RoleName = $"{appName}RDSMonitoringRole",
-                ManagedPolicies = new[] {
+                ManagedPolicies = [
                     ManagedPolicy.FromAwsManagedPolicyName("service-role/AmazonRDSEnhancedMonitoringRole")
-                }
+                ]
             });
 
-            DatabaseInstance instance = new DatabaseInstance(this, $"{appName}DatabaseInstance", new DatabaseInstanceProps {
+            DatabaseInstance instance = new(this, $"{appName}DatabaseInstance", new DatabaseInstanceProps {
                 InstanceIdentifier = $"{appName}PostgreSQLInstance",
                 Engine = DatabaseInstanceEngine.Postgres(new PostgresInstanceEngineProps {
                     Version = PostgresEngineVersion.VER_17_2
@@ -84,7 +97,27 @@ namespace BDiazErdsPostgreSql
                 MonitoringRole = role,
                 CloudwatchLogsExports = ["postgresql"],
                 CloudwatchLogsRetention = RetentionDays.ONE_MONTH,
+                RemovalPolicy = RemovalPolicy.RETAIN,
             });
+
+            // Se crea parámetro con información de conexión a la base de datos...
+            _ = new Secret(this, $"{appName}RDSPostgreSQLSecret", new SecretProps { 
+                SecretName = $"/{appName}/RDSPostgreSQL/ConnectionString",
+                Description = $"Connection String de la base de datos RDS PostgreSQL de la aplicación {appName}",
+                SecretObjectValue = new Dictionary<string, SecretValue> {
+                    { "Host", SecretValue.UnsafePlainText(instance.DbInstanceEndpointAddress) },
+                    { "Port", SecretValue.UnsafePlainText(instance.DbInstanceEndpointPort) },
+                    { "MasterUser", SecretValue.UnsafePlainText(masterUsername) },
+                    { "MasterPassword", SecretValue.UnsafePlainText(masterPassword) },
+                    // Se añaden las passwords de las subapps...
+                    { $"{subapp01Name}Database", SecretValue.UnsafePlainText(subapp01Database) },
+                    { $"{subapp01Name}Username", SecretValue.UnsafePlainText(subapp01Username) },
+                    { $"{subapp01Name}Password", SecretValue.UnsafePlainText(subapp01Password) },
+                    { $"{subapp02Name}Database", SecretValue.UnsafePlainText(subapp02Database) },
+                    { $"{subapp02Name}Username", SecretValue.UnsafePlainText(subapp02Username) },
+                    { $"{subapp02Name}Password", SecretValue.UnsafePlainText(subapp02Password) },
+                },
+             });
         }
     }
 }
--- src/BDiazErdsPostgreSql/Program.cs
+++ src/BDiazErdsPostgreSql/Program.cs
@@ -14,8 +14,7 @@ namespace BDiazErdsPostgreSql
             string region = System.Environment.GetEnvironmentVariable("REGION_AWS")!;
 
             var app = new App();
-            new BDiazERDSPostgreSqlStack(app, $"Cdk{appName}RDSPostgreSQL", new StackProps
-            {
+            _ = new BDiazERDSPostgreSqlStack(app, $"Cdk{appName}RDSPostgreSQL", new StackProps {
                 // If you don't specify 'env', this stack will be environment-agnostic.
                 // Account/Region-dependent features and context lookups will not work,
                 // but a single synthesized template can be deployed anywhere.
