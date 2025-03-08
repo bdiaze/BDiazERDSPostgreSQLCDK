8777b5cdac521a5f4c81457c4b7d9d1114f9e266
Benjamín <benjamin.diaze@outlook.com>
2025-03-08 11:20:56 -0300

    Se añaden logs en initial creation lambda

--- InitialCreationLambda/InitialCreationLambda/Function.cs
+++ InitialCreationLambda/InitialCreationLambda/Function.cs
@@ -2,6 +2,7 @@ using Amazon.Lambda.Core;
 using MateoAPI.Helpers;
 using Newtonsoft.Json;
 using Npgsql;
+using System.Diagnostics;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -17,13 +18,20 @@ public class Function {
     /// <param name="context"></param>
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context) {
+        Stopwatch sw = Stopwatch.StartNew();
+        LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Iniciando proceso de creación inicial de la base de datos y sus usuarios administradores...");
+
         string secretArnConnectionString = Environment.GetEnvironmentVariable("SECRET_ARN_CONNECTION_STRING") ?? throw new ArgumentNullException("SECRET_ARN_CONNECTION_STRING");
         string subapp01Name = Environment.GetEnvironmentVariable("SUBAPP_01_NAME") ?? throw new ArgumentNullException("SUBAPP_01_NAME");
         string subapp02Name = Environment.GetEnvironmentVariable("SUBAPP_02_NAME") ?? throw new ArgumentNullException("SUBAPP_02_NAME");
 
+        LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Obteniendo secreto de conexión a base de datos...");
+
         Dictionary<string, string> connectionString = SecretManager.ObtenerSecreto(secretArnConnectionString).Result;
 
         List<string> retorno = [];
+
+        LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Conectandose a base de datos RDS PostgreSQL [{connectionString["Host"]}]...");
 
         using (NpgsqlConnection conn = new(
             $"Server={connectionString["Host"]};Port={connectionString["Port"]};SslMode=prefer;" +
@@ -33,11 +41,12 @@ public class Function {
 
             // Se crea database subapp02...
             string subapp02Database = connectionString[$"{subapp02Name}Database"];
+            LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Creando base de datos para subapp02 \"{subapp02Name}\" - [Base de Datos: {subapp02Database}]...");
             try {
                 using NpgsqlCommand cmd = new($"CREATE DATABASE \"{subapp02Database}\"", conn);
                 cmd.ExecuteNonQuery();
             } catch (Exception ex) {
-                string mensaje = "Error al crear base de datos de la subapp02: " + ex;
+                string mensaje = $"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Error al crear base de datos de la subapp02: " + ex;
                 LambdaLogger.Log(mensaje);
                 retorno.Add(mensaje);
             }
@@ -45,25 +54,29 @@ public class Function {
             // Se crea usuario administrador subapp02...
             string subapp02AdmUsername = connectionString[$"{subapp02Name}AdmUsername"];
             string subapp02AdmPassword = connectionString[$"{subapp02Name}AdmPassword"];
+            LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Creando usuario administrador para subapp02 \"{subapp02Name}\"...");
             try {
                 using NpgsqlCommand cmd = new($"CREATE USER \"{subapp02AdmUsername}\" WITH ENCRYPTED PASSWORD '{subapp02AdmPassword}' CREATEROLE", conn);
                 cmd.ExecuteNonQuery();
             } catch (Exception ex) {
-                string mensaje = "Error al crear usuario administrador de la subapp02: " + ex;
+                string mensaje = $"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Error al crear usuario administrador de la subapp02: " + ex;
                 LambdaLogger.Log(mensaje);
                 retorno.Add(mensaje);
             }
 
             // Se otorgan permisos al nuevo usuario administrador subapp02 sobre la base creada...
+            LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Otorgando permisos a usuario administrador para subapp02 \"{subapp02Name}\"...");
             try {
                 using NpgsqlCommand cmd = new($"GRANT ALL PRIVILEGES ON DATABASE \"{subapp02Database}\" TO \"{subapp02AdmUsername}\" WITH GRANT OPTION", conn);
                 cmd.ExecuteNonQuery();
             } catch (Exception ex) {
-                string mensaje = "Error al otorgar permisos al usuario administrador de la subapp02: " + ex;
+                string mensaje = $"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Error al otorgar permisos al usuario administrador de la subapp02: " + ex;
                 LambdaLogger.Log(mensaje);
                 retorno.Add(mensaje);
             }
         }
+
+        LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Ha terminado el proceso de creación inicial de la base de datos y sus usuarios administradores...");
 
         return JsonConvert.SerializeObject(retorno);
     }
